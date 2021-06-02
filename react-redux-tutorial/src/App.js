e710f6a414898feb5738f7862fd19b9e89013610
dylee <leeduyoung2002@gmail.com>
2021-06-02 23:30:57 +0900

    feat: Add redux

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterContainer />
+      <hr />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
