5aad3a50ed07b0850fff5feb0d92d6c461a21741
kaye <kaye.lee@luxrobo.com>
2021-06-03 11:49:31 +0900

    feat: useSelector, useDispatch를 사용하여 리덕스 편하게 사용하기

--- react-redux-tutorial-upgrade/src/containers/CounterContainer.js
+++ react-redux-tutorial-upgrade/src/containers/CounterContainer.js
@@ -1,72 +1,32 @@
 // 리덕스 스토어와 연동된 컴포넌트를 컨테이너 컴포넌트라고 부른다.
 
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  /**
+   * useSelector Hook을 사용하여 connect 함수를 사용하지 않고도 리덕스의 상태를 조회할 수 있다.
+   */
+  const number = useSelector((state) => state.counter.number);
+
+  /**
+   * useDispatch Hook을 사용하여 컴포넌트 내부에서 스토어의 내장함수 dispatch를 사용할 수 있다.
+   */
+  const dispatch = useDispatch();
+
+  // 성능 최적화
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-// CASE 1
-// const mapStateToProps = (state) => ({
-//   number: state.counter.number,
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   increase: () => {
-//     dispatch(increase());
-//   },
-//   decrease: () => {
-//     dispatch(decrease());
-//   },
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
-// CASE 2
-// export default connect(
-//   (state) => ({
-//     number: state.counter.number,
-//   }),
-//   (dispatch) => ({
-//     increase: () => dispatch(increase()),
-//     decrease: () => dispatch(decrease()),
-//   }),
-// )(CounterContainer);
-
-// CASE 3
-// export default connect(
-//   (state) => ({
-//     number: state.counter.number,
-//   }),
-//   (dispatch) => bindActionCreators({ increase, decrease }, dispatch),
-// )(CounterContainer);
-
-// CASE 4
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
-
 /**
- * connect(mapStateToProps, mapDispatchToProps)(연동할 컴포넌트)
- *
- * mapStateToProps
- * : 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
- *
- * mapDispatchToProps
- * : 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
- *
- * connect 함수를 호출하면 또 다른 함수를 반환한다. 반환된 함수에 컴포넌트를 파라미터로 넣어주면
- * 리덕스와 연동된 컴포넌트가 만들어진다.
+ * connect 함수를 사용할 경우 컨테이너의 props의 변경이 없을 경우 리렌더링을 자동으로 방지하여 성능이 최적화 되지만,
+ * useSelector를 사용하여 리덕스 상태를 조회했을때는 매번 다시 렌더링하기때문에 React.memo를 사용하여 최적화해주어야한다.
  */
+export default React.memo(CounterContainer);
